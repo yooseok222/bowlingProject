33167ac1d72ea1831d43dc745028900d8e7d7a01
yooseok222 <yooseok21@naver.com>
2025-03-07 14:19:16 +0900

    add: Lane 객체 생성

--- src/kr/kosa/bowl/Run.java
+++ src/kr/kosa/bowl/Run.java
@@ -4,8 +4,11 @@ public class Run {
 
 	public static void main(String[] args) {
 
-		Manager m = new Manager();
-		m.validateManager();
+		//Manager m = new Manager();
+		//m.validateManager();
+		
+		Lane lane = new Lane();
+		System.out.println(lane);
 		
 	}
 
